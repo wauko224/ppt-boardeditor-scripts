97a48021349c62bd0d8716ee43066d65cfa64ff3
wauko224 <i.chikatomi1016@docomo.ne.jp>
2025-04-07 15:15:35 +0900

    lo

--- loop_I-onsteamdeck.py
+++ loop_I-onsteamdeck.py
@@ -1,4 +1,9 @@
-rm -rf PPTBoardEditor
-git clone https://github.com/ZetrisAI/PPT-BoardEditor.git
-cd PPT-BoardEditor
-ls
+cd ~
+mkdir mono-local
+cd mono-local
+
+# Linux用のMonoバイナリをダウンロード（例: Ubuntu 20.04用の6.12.0）
+wget https://download.mono-project.com/archive/6.12.0/linux/x64/mono-6.12.0.122.tar.xz
+
+# 解凍
+tar -xvJf mono-6.12.0.122.tar.xz
