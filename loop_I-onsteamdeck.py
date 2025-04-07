de88f6bda0f25367182581d6f2a63c25e759125f
wauko224 <i.chikatomi1016@docomo.ne.jp>
2025-04-07 15:29:43 +0900

    Update loop_I-onsteamdeck.py

--- loop_I-onsteamdeck.py
+++ loop_I-onsteamdeck.py
@@ -1,9 +1,1 @@
-cd ~
-mkdir mono-local
-cd mono-local
-
-# Linux用のMonoバイナリをダウンロード（例: Ubuntu 20.04用の6.12.0）
-wget https://download.mono-project.com/archive/6.12.0/linux/x64/mono-6.12.0.122.tar.xz
-
-# 解凍
-tar -xvJf mono-6.12.0.122.tar.xz
+flatpak install --user org.freedesktop.Sdk.Extension.mono6
